68f453e64d122554f83f6ba9eb0cb4a47142ca1d
Isaac <isaacandrescr@gmail.com>
2026-01-08 21:41:31 -0500

    segundo commit

--- practica1/src/App.tsx
+++ practica1/src/App.tsx
@@ -1,19 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+    <div className='Main'>
       <h1>
         HOla mundo 
       </h1>
-      <button>
+      <button className='Isaac'>
         Pagina Isaac
-      </button> 
+      </button>
+    </div> 
     </>
   )
 }
