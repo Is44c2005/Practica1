40a8de7eaef9ea2d193fbe4ab0611d68a041f545
MateoCueva28 <mateocueva5a@gmail.com>
2026-01-27 18:51:56 -0500

    Primer commit

--- practica1/src/App.tsx
+++ practica1/src/App.tsx
@@ -6,13 +6,16 @@ function App() {
     <>
     <div className='Main'>
       <h1>
-        HOla mundo 
+        Hola mundo 
       </h1>
       <button className='Isaac'>
         Pagina Isaac
       </button>
       <button className='Mateo'>
         Pagina Mateo
+      </button>
+      <button className='Agujin'>
+        Pagina Agujin
       </button>
     </div> 
     </>
