9bc4954bc25a519c1a41884faf2058218f31a2b2
Alejandro777-2 <flavio.ibujes@udla.edu.ec>
2026-01-27 18:42:12 -0500

    FireBase

--- practica1/src/App.tsx
+++ practica1/src/App.tsx
@@ -1,21 +1,22 @@
 import './App.css'
 
 function App() {
-
   return (
-    <>
     <div className='Main'>
-      <h1>
-        HOla mundo 
-      </h1>
+      <h1>Hola mundo</h1> 
+
       <button className='Isaac'>
         Pagina Isaac
       </button>
+
       <button className='Mateo'>
         Pagina Mateo
       </button>
+
+      <button className='Agujin'>
+        Pagina Agujin
+      </button>
     </div> 
-    </>
   )
 }
 
