c63537051ac6cd562466db2d548d29d362c36894
MateoCueva28 <mateocueva5a@gmail.com>
2026-01-28 16:08:57 -0500

    Rutas actualizadas

---
+++ practica1/src/screens/Agujin.tsx
@@ -0,0 +1,10 @@
+function Agujin() {
+  return (
+    <div>
+      <h1>Agujin</h1>
+      <p>Página personal</p>
+    </div>
+  );
+}
+
+export default Agujin;
---
+++ practica1/src/screens/Isaac.tsx
@@ -0,0 +1,22 @@
+function Isaac() {
+  return (
+    <div>
+      <h1>Isaac</h1>
+      <h2>Estudiante de Ingeniería</h2>
+
+      <p>
+        Me interesa el desarrollo de software, programación en Java y
+        desarrollo web con React.
+      </p>
+
+      <h3>Habilidades</h3>
+      <ul>
+        <li>Java</li>
+        <li>React</li>
+        <li>Git y GitHub</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Isaac;
